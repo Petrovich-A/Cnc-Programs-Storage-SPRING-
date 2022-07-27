89247e02029be72b642d5dbb572c85c5f29b014d
Petrovich-A <A.Piatrovich@gmail.com>
2022-07-27 08:32:32 +0300

    Spring Boot + Spring data JPA + MySQL app

---
+++ src/test/java/by/petrovich/CncProgramsStorage/CncProgramsStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package by.petrovich.CncProgramsStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CncProgramsStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
