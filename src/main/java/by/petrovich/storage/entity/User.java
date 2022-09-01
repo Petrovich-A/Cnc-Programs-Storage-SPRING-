7bfca4aef26058f86c73e7f723df10cfecc2b1d3
Alexandr <a.piatrovich@gmail.com>
2022-09-01 22:42:19 +0300

    add Hibernate dependency

--- src/main/java/by/petrovich/storage/entity/User.java
+++ src/main/java/by/petrovich/storage/entity/User.java
@@ -8,7 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-//@Table(name = "user")
+@Table(name = "users")
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
